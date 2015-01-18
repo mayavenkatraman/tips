76c0b60157f234212ea58d1d63ca3eea851acbdb
mayav <mayav@yahoo-inc.com git config --global user.name mayavenkaraman git config --global user.email mayav@yahoo-inc.com>
2015-01-18 15:38:05 -0800

    now with the two point decimal working

--- ViewController.swift
+++ ViewController.swift
@@ -14,14 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        tipLabel.text = "$ 0.0"
-        totalLabel.text = "$ 0.0"
-        
-    
+       
+        tipLabel.text = "$ 0.00"
+        totalLabel.text = "$ 0.00"
     }
    
 
@@ -30,7 +28,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-   
-
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var billAmount = NSString(string: billField.text).doubleValue
+    var tip = billAmount * 0.2
+    var total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+    
+        tipLabel.text = String(format: "$%.2f", tip)
+          totalLabel.text = String(format: "$%.2f", total)
 }
 
+}
